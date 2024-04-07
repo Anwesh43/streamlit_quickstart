7d86eeb1918cde828374672dbbed59ec1c3dcf52
Anwesh43 <anweshthecool0@gmail.com>
2024-04-07 23:59:57 +0530

    adding dataframe sum, std

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd 
+import numpy as np 
 
 df = pd.DataFrame({
     "a" : [10, 20, 30, 40],
@@ -9,3 +10,15 @@ df = pd.DataFrame({
 
 #st.write(df)
 st.dataframe(df)
+
+st.dataframe(df.sum())
+
+st.dataframe(df.sum(axis=1))
+
+st.dataframe(df.std())
+st.dataframe({
+    "sum_stack": [df.stack().sum()],
+    "np_sum": [np.sum(df.values)]
+})
+
+st.dataframe(df.stack())
