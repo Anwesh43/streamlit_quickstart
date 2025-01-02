d4e100bacea6f0dc01eb6ed3dc34f64dba39ec43
Anwesh43 <anweshthecool0@gmail.com>
2025-01-02 19:26:34 +0530

    adding st.empty demo using with

--- demo_placeholder.py
+++ demo_placeholder.py
@@ -19,5 +19,11 @@ def showCaptionOnClick():
         # time.sleep(2)
         st.caption(input)
 
+with st.empty():
+    for i in range(0, 5):
+        st.markdown("Count {}".format(i + 1))
+        time.sleep(1)
+    st.markdown("Stopping counter")
+
 showCaptionOnClick()
 createPlaceHolder()
